dc22cae06f68a29c6a8b4722b6268b0575c96389
ricoloic <ricoloic4@gmail.com>
2024-01-23 02:19:35 -0500

    added more base packages

--- init_setup.sh
+++ init_setup.sh
@@ -8,7 +8,7 @@ mkdir ~/personal
 mkdir ~/programs
 
 # installing small nice to have package
-sudo apt install -y curl neofetch htop git make cmake build-essential python3 tmux
+sudo apt install -y curl neofetch htop git make cmake build-essential python3 tmux btop tree docker ffmpeg
 
 # clonning and installing Kickstart.nvim
 sudo chmod +x ./nvim_setup.sh
